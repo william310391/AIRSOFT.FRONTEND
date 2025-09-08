6caf45a0c1b8f2270dcc0a0a6453cf7f5d260daf
William Astucuri <william310391@gmail.com>
2025-09-08 00:28:11 -0500

    ajuste cerrar session

--- src/app/features/error/services/error.service.ts
+++ src/app/features/error/services/error.service.ts
@@ -1,12 +1,14 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { HTTP_STATUS_CODES, HttpStatus } from '../constants/HTTP_STATUS_CODES';
+import { LayoutService } from 'src/app/core/layout/services/layout.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ErrorService {
   constructor(private router: Router) {}
+  layoutService = inject(LayoutService);
   getStatus(code: number | string): HttpStatus {
     return (
       HTTP_STATUS_CODES[code.toString()] || {
@@ -19,7 +21,7 @@ export class ErrorService {
 
   volver(codeError: number | null) {
     if (codeError == 401 || codeError == null) {
-      this.router.navigate(['/login']);
+      this.layoutService.cerrarSesion();
     } else {
       this.router.navigate(['/prueba']);
     }
